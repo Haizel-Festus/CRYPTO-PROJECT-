ef0cd200bb266da64ba36fc73f2d1e096e934076
Haizel Abaka Festus <haizelabakafestus19@proton.me>
2024-12-08 09:42:54 +0000

    Add project files for Crypto Project

---
+++ Assest/JAVASCRIPT/main-script.js
@@ -0,0 +1,7 @@
+const hambugerButton = document.getElementById("hambunger");
+const listMenu = document.getElementById("navMenu");
+
+ hambugerButton.addEventListener("click", () =>{
+     listMenu.classList.toggle("new-header");
+})
+
